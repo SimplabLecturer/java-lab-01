d0b00a9f5af325c6dcc66d407b7ea6c2a5602d98
Albert Einstein <83507358+SimplabLecturer@users.noreply.github.com>
2021-06-13 17:27:26 +0300

    Update CommonTasks.java

--- src/main/java/com/simplab/testdata/CommonTasks.java
+++ src/main/java/com/simplab/testdata/CommonTasks.java
@@ -2,18 +2,22 @@ package com.simplab.testdata;
 
 public class CommonTasks {
     public int sum(int a, int b) {
+        System.out.println("To be implemented");
         return 0;
     }
 
     public int product(int a, int b) {
+        System.out.println("To be implemented");
         return 0;
     }
 
     public int division(int a, int b) {
+        System.out.println("To be implemented");
         return 0;
     }
 
     public int pow(int a, int b) {
+        System.out.println("To be implemented");
         return 0;
     }
 }
